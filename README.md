4ab4e013d41e38342a3828dc2eb3c31194a7958f
ThomasRogersF <thomasrogersfiguera@gmail.com>
2023-07-06 21:17:47 -0400

    First table stable
    Unit 1 - A0 - El Abecedario

---
+++ README.md
@@ -0,0 +1,6 @@
+# Foobar
+
+SpanishVIP Academy notes
+
+assets will be organized on folders divided by its unit
+
--- js/actions.js
+++ js/actions.js
@@ -1,19 +1,24 @@
+
+
+//Audio tables icon clickable
 $(document).ready(function() {
-    //Audio 1 Starts
-    var audio1 = new Audio('assets/02 - Personal Pronouns.mp3')
-    $(".recording1").mousedown(function() {
-      audio1.load();
-      audio1.play();
-    });
-    //Audio 1 Ends
 
-    //Audio 2 Starts
-    var audio2 = new Audio('assets/03 - Personal Pronouns.mp3')
-    $(".recording2").mousedown(function() {
-      audio2.load();
-      audio2.play();
-    });
-    //Audio 2 Ends
+  var audios = [];
 
+  $(".recording").click(function() {
+
+      var audioSrc = $(this).data('audioSrc');
+
+      // Stop all other audios
+      for (var i = 0; i < audios.length; i++) {
+          audios[i].pause();
+          audios[i].currentTime = 0;
+      }
+
+      var audio = new Audio(audioSrc);
+      audios.push(audio);
+      audio.play();
 
   });
+
+});
